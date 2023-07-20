3735b7ba0ef0e43a753f74eb785fca6e6ab7550f
郑成琦 <zhen0144@e.ntu.edu.sg>
2023-07-20 16:00:53 +0800

    Add new function to find curve

--- src/main_ui.py
+++ src/main_ui.py
@@ -1,19 +1,25 @@
 # %%
 import sys
 
-import cv2
 from PySide6 import QtCore, QtWidgets, QtGui
 from src.Dataloader import Dataloader
 import matplotlib.pyplot as plt
 from PIL import Image, ImageQt
 import numpy as np
-from utils.transfer_coordinate_system import cartesian_to_polar
+from utils.dijkstra import shortest_path
+
+
+def open_image_by_plt(image_data: np.array):
+    plt.imshow(image_data)
+    plt.show()
 
 
 class DataloaderUi(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
+        self.shortest_path_list = None
+        self.anchor_list = None
         self.file_name = None
         self.dataloader = Dataloader()
         self.center_x = None
@@ -40,6 +46,7 @@ class DataloaderUi(QtWidgets.QWidget):
 
         # >>>> second line >>>>
 
+        # <<<< Slider part <<<<
         self.slider_adjust_width = QtWidgets.QSlider()
         self.slider_adjust_width.setOrientation(QtCore.Qt.Orientation.Horizontal)
         self.slider_adjust_width.valueChanged.connect(self.resize_the_image)
@@ -47,7 +54,21 @@ class DataloaderUi(QtWidgets.QWidget):
         self.layout_slider = QtWidgets.QHBoxLayout()
         self.layout_slider.addWidget(self.label_of_slider_number)
         self.layout_slider.addWidget(self.slider_adjust_width)
-        # self.slider_adjust_width.valueChanged.connect()
+        # >>>> Slider part >>>>
+
+        # >>>> anchor part >>>>
+        self.button_set_anchor = QtWidgets.QPushButton("Press to open image shower")
+        self.button_find_curve_polar = QtWidgets.QPushButton("Find Curve")
+        self.lineedit_anchor = QtWidgets.QLineEdit()
+        self.layout_anchor = QtWidgets.QHBoxLayout()
+        self.layout_anchor.addWidget(self.button_set_anchor)
+        self.layout_anchor.addWidget(self.lineedit_anchor)
+        self.layout_anchor.addWidget(self.button_find_curve_polar)
+        # <<<< anchor part <<<<
+
+        # >>>> finding ellipse part >>>>
+        self.button_show_ellipse = QtWidgets.QPushButton("Show ellipse on the image")
+        # <<<< finding ellipse part <<<<
 
         # <<<<<  Layout setting  <<<<<
         self.layout_main = QtWidgets.QVBoxLayout(self)
@@ -59,6 +80,8 @@ class DataloaderUi(QtWidgets.QWidget):
         self.button_popup_plt_to_selection_center.clicked.connect(self.popup_plt_image)
         self.button_transform_coordinate_cartesian_to_polar.clicked.connect(
             self.transform_coordinate_cartesian_to_polar)
+        self.button_set_anchor.clicked.connect(self.open_reside_image_for_anchor)
+        self.button_find_curve_polar.clicked.connect(self.find_path)
 
     @QtCore.Slot()
     def open_file_list(self):
@@ -125,18 +148,48 @@ class DataloaderUi(QtWidgets.QWidget):
         self.label_of_slider_number.setText(f"Width: {self.slider_adjust_width.value()}")
         self.layout_main.addLayout(self.layout_slider)
 
+        self.layout_main.addLayout(self.layout_anchor)
+
     def resize_the_image(self):
         new_width = self.slider_adjust_width.value()
         self.label_of_slider_number.setText(f"Width: {new_width}")
         new_size = [int(new_width), self.dataloader.polar_image.shape[0]]
         self.dataloader.resize_image(original_image=self.dataloader.original_image, new_size=new_size)
-        new_center = [int(self.center_x), int(self.center_y * new_width // self.dataloader.original_image.shape[1])]
+        new_center = [int(self.center_x * new_width // self.dataloader.original_image.shape[0]), int(self.center_y)]
         print(new_center)
         self.dataloader.transform_car_to_polar(
             center=[new_center[0], new_center[1]],
             src=self.dataloader.resized_image,
             radius=int(self.lineEdit_radius.text()))
         self.show_image(image_data=self.dataloader.polar_image, temp_file_name="resize_image.jpg")
+
+    def open_reside_image_for_anchor(self):
+        fig = plt.gcf()
+        open_image_by_plt(self.dataloader.polar_image)
+        cid = fig.canvas.mpl_connect('button_press_event', self.set_anchor)
+        self.anchor_list = []
+
+    def set_anchor(self, event):
+        if event.xdata is not None and event.ydata is not None:
+            y = event.xdata
+            self.anchor_list.append(y)
+            print(self.anchor_list)
+
+    def find_path(self):
+        assert self.anchor_list is not None
+        self.shortest_path_list = []
+        for index_begin_point in range(len(self.anchor_list)):
+            begin_point = int(self.anchor_list[index_begin_point])
+            region_y = (-10, 10)
+            sub_image = self.dataloader.polar_image[:, begin_point + region_y[0]:begin_point + region_y[1]]
+            path, seen = shortest_path(img_data=sub_image, begin=(0, 10), end=(511, 10))
+            self.shortest_path_list.append(np.array(path) + [0, begin_point - 10])
+        canva = np.zeros_like(self.dataloader.polar_image)
+        for path in self.shortest_path_list:
+            for x, y in path:
+                canva[x, y] = 0.2
+        plt.imshow(canva + self.dataloader.polar_image)
+        plt.show()
 
 
 if __name__ == "__main__":
